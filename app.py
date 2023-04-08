78774c0d6cabcf72bb27078da680fbe719d51d5b
Krish C Naik <krishnaik06@gmail.com>
2023-04-08 23:02:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
